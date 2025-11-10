a2c201dcefe26fdc2c1d3452334def92fffaa3bd
Next Claim <nextclaimmd@gmail.com>
2025-11-11 00:20:48 +0500

    added new lint

--- src/sanity/lib/client.js
+++ src/sanity/lib/client.js
@@ -8,3 +8,16 @@ export const client = createClient({
   apiVersion,
   useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
+
+import { createClient } from "next-sanity";
+
+if (!process.env.NEXT_PUBLIC_SANITY_PROJECT_ID) {
+  console.warn("❌ Missing NEXT_PUBLIC_SANITY_PROJECT_ID");
+}
+
+export const client = createClient({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "ocfze6yd" ,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2025-02-19",
+  useCdn: true,
+});
