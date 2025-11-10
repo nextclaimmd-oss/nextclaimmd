684543430082dd22a95a8e4643a787f02ebd52c4
Next Claim <nextclaimmd@gmail.com>
2025-11-11 00:35:57 +0500

    added new config

--- src/sanity/lib/client.js
+++ src/sanity/lib/client.js
@@ -1,14 +1,3 @@
-import { createClient } from 'next-sanity'
-
-import { apiVersion, dataset, projectId } from '../env'
-
-export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
-  useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
-})
-
 import { createClient } from "next-sanity";
 
 if (!process.env.NEXT_PUBLIC_SANITY_PROJECT_ID) {
